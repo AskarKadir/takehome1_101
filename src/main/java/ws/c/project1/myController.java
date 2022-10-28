d196cea52ed79bfeb801e49861c28ef4060cfdd9
AskarKadir <askarkadir07@gmail.com>
2022-10-28 13:29:27 +0700

    membuat rangka pengkondisian data

--- src/main/java/ws/c/project1/myController.java
+++ src/main/java/ws/c/project1/myController.java
@@ -4,11 +4,34 @@
  */
 package ws.c.project1;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 /**
  *
  * @author Asus
  */
-@org.springframework.stereotype.Controller
+@Controller
 public class myController {
     
+    @RequestMapping("/input")
+    public String olahData(
+            @RequestParam(value = "namabarang")String Newnamabarang,
+            @RequestParam(value = "jumlahbarang") String Newjumlahbarang,
+            @RequestParam(value = "totalharga") String Newtotalharga,
+            Model model)
+    {
+        String total;
+        if (Newjumlahbarang.isEmpty() && 
+                Newnamabarang.isEmpty() && 
+                Newtotalharga.isEmpty())
+        {
+            return "dataeror";
+        }else{
+            
+        }
+        return "pembayaran";
+    }
 }
