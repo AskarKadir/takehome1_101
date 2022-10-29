28424d234ad8974a683741e69efb447cdc0280f3
AskarKadir <askarkadir07@gmail.com>
2022-10-29 12:29:00 +0700

    memperbaiki kondisi diskon antara 10K - 49K

--- src/main/java/ws/c/project1/myController.java
+++ src/main/java/ws/c/project1/myController.java
@@ -38,7 +38,7 @@ public class myController {
             if (Double.parseDouble(total) < 10000 ) {
                 diskon = "0";
                 total = String.valueOf(Double.parseDouble(total) - (Double.parseDouble(total) *  Double.parseDouble(diskon)/100));
-            }else if (Double.parseDouble(total) >= 10000){
+            }else if (Double.parseDouble(total) >= 10000 && Double.parseDouble(total) < 50000){
                 diskon = "5";
                 total = String.valueOf(Double.parseDouble(total) - (Double.parseDouble(total) *  Double.parseDouble(diskon)/100));
             }else if (Double.parseDouble(total) >= 50000){
